7a2f104a7ad0909539499ed4a0f41aa5c24a3c71
hariboten <botegineer@outlook.com>
2021-02-12 11:58:05 +0900

    sh_launch_child(), argument norm error fix. close #7

--- src/sh_launch.c
+++ src/sh_launch.c
@@ -6,21 +6,23 @@
 /*   By: ewatanab <ewatanab@student.42tokyo.jp      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/01 14:45:21 by ewatanab          #+#    #+#             */
-/*   Updated: 2021/02/12 11:49:07 by ewatanab         ###   ########.fr       */
+/*   Updated: 2021/02/12 11:56:50 by ewatanab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/sh_launch.h"
 
-void	sh_launch_child(t_minishell *m_sh, t_exec *exec_param, int *pipefd, int prev_pipe, bool has_next)
+void	sh_launch_child(t_minishell *m_sh, t_list *exlist, int *pipefd, int prev_pipe)
 {
 	t_builtin_f	builtin_function;
+	t_exec		*exec_param;
 
+	exec_param = exlist->content;
 	if (prev_pipe)
 		sh_dup_close(prev_pipe, 0);
 	if (exec_param->fd_in != 0)
 		sh_dup_close(exec_param->fd_in, 0);
-	if (has_next)
+	if (exlist->next)
 		sh_dup_close(pipefd[1], 1);
 	if (exec_param->fd_out != 1)
 		sh_dup_close(exec_param->fd_out, 1);
@@ -38,15 +40,13 @@ int		sh_process_manager(t_minishell *m_sh, t_list *execlist, int prev_pipe)
 	pid_t	cpid;
 	int		status;
 	int		pipefd[2];
-	t_exec	*exec_param;
 
-	exec_param = execlist->content;
 	if (execlist->next && pipe(pipefd) < 0)
 		return (ft_perror("minishell"));
 	if ((cpid = fork()) < 0)
 		return (ft_perror("minishell"));
 	if (cpid == 0)
-		sh_launch_child(m_sh, exec_param, pipefd, prev_pipe, (execlist->next != NULL));
+		sh_launch_child(m_sh, execlist, pipefd, prev_pipe);
 	if (waitpid(cpid, &status, 0) < 0)
 		return (ft_perror("minishell"));
 	m_sh->exit_status = WEXITSTATUS(status);
