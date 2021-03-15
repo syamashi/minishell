917e2f82e149d66a3a147dd91a15a64821b98496
syamashi <syamashi@student.42tokyo.jp>
2021-03-15 23:22:10 +0900

    NULL input ret fix

--- src/sh_launch.c
+++ src/sh_launch.c
@@ -6,7 +6,7 @@
 /*   By: syamashi <syamashi@student.42.tokyo>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/01 14:45:21 by ewatanab          #+#    #+#             */
-/*   Updated: 2021/03/15 22:40:25 by syamashi         ###   ########.fr       */
+/*   Updated: 2021/03/15 23:21:43 by syamashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,7 +84,7 @@ int		sh_launch(t_minishell *m_sh, t_list *execlist)
 	t_builtin_f	builtin_function;
 
 	if (!((t_exec *)execlist->content)->argv[0])
-		return (m_sh->exit_status = 0);
+		return (m_sh->exit_status);
 	if (!execlist->next && (builtin_function = builtin_table(execlist->content)))
 	{
 		if (((t_exec *)execlist->content)->error_flag)
