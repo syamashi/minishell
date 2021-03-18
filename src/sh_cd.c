5f3f218427386d6c7efb11d7c8d4f6d8af2ba812
syamashi <syamashi@student.42tokyo.jp>
2021-03-18 15:16:15 +0900

    #182 cd

--- src/sh_cd.c
+++ src/sh_cd.c
@@ -6,7 +6,7 @@
 /*   By: syamashi <syamashi@student.42.tokyo>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/17 18:23:23 by syamashi          #+#    #+#             */
-/*   Updated: 2021/03/17 23:11:29 by syamashi         ###   ########.fr       */
+/*   Updated: 2021/03/18 15:16:01 by syamashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int		cd_no_args(t_minishell *m_sh, int fd)
 		return (pwd_update(m_sh, path, false));
 	}
 	if (chdir(home) == -1)
-		return (ft_cd_free_error(home, 1, MYENOENT, fd));
+		return (ft_cd_free_error(home, 1, strerror(errno), fd));
 	ft_lstclear(&m_sh->pwds, free);
 	pwd_update(m_sh, home, false);
 	free(home);
@@ -36,7 +36,7 @@ int		cd_no_current(t_minishell *m_sh, char *argv, int fd)
 {
 	pwd_update(m_sh, argv, NOCURRENT);
 	ft_putstr_fd("cd: error retrieving current directory: ", fd);
-	ft_putstr_fd("getcwd: cannot access parent directories : ", fd);
+	ft_putstr_fd("getcwd: cannot access parent directories: ", fd);
 	ft_putstr_fd("No such file or directory\n", fd);
 	return (0);
 }
@@ -58,7 +58,7 @@ int		cd_blank_args(t_minishell *m_sh, char *path, char *argv, int fd)
 	if (chdir(input_pwd))
 	{
 		free(input_pwd);
-		return (cd_error(argv, 1, MYENOENT, fd));
+		return (cd_error(argv, 1, strerror(errno), fd));
 	}
 	free(input_pwd);
 	return (pwd_update(m_sh, argv, false));
@@ -79,7 +79,7 @@ int		sh_cd(t_minishell *m_sh, t_exec *exec)
 	if (!**argv && (path = getcwd(NULL, 0)))
 		return (cd_blank_args(m_sh, path, *argv, exec->fd_err));
 	if (chdir(*argv))
-		return (cd_error(*argv, 1, MYENOENT, exec->fd_err));
+		return (cd_error(*argv, 1, strerror(errno), exec->fd_err));
 	path = getcwd(NULL, 0);
 	if (!path && (nocurrent = true))
 		return (cd_no_current(m_sh, *argv, exec->fd_err));
